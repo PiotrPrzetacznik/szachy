5a34b1635a62c3b854cc5cf7027c0621d68f7451
piotr <231298@stud.usz.edu.pl>
2025-01-25 23:14:54 +0100

    start

---
+++ src/main/java/com/example/szachy/SzachyApplication.java
@@ -0,0 +1,13 @@
+package com.example.szachy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzachyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SzachyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/szachy/SzachyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.szachy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzachyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
